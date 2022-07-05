67c651e4101afd3d5c3e964ec39a8d595885e00c
thanhdanh27600 <thanhdanh27600@gmail.com>
2022-07-05 23:43:06 +0700

    init schema & migration

---
+++ script.ts
@@ -0,0 +1,20 @@
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+	// const user = await prisma.user.create({
+	// 	data: {
+	// 		name: "Phu Huy",
+	// 	},
+	// });
+	const user = await prisma.user.findMany();
+	console.log(user);
+}
+
+main()
+	.catch((e) => {
+		console.error(e.message);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
